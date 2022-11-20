aa7dcc0a13bb7233b6156c30cc2823f388dd53da
Michaela Brown <m_brownbrown@yahoo.com>
2022-11-19 16:13:34 -0800

    commit tree 1b906474b8540af40bdbea66371e8c5056af4176

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let first = string[0];
+    let last = string.substring(1);
+    let firstUpper = first.toUpperCase();
+    return firstUpper + last;
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '')
+  }
+
+  static titleize(string) {
+    let littleWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newString = string.split(' ');
+  let finalString = [];
+    for (let word in newString) {
+      if (littleWords.indexOf(newString[word]) > -1) {
+        finalString.push(newString[word]);
+      } else if (littleWords.indexOf(newString[word]) <= -1) {
+        newString[word] = newString[word][0].toUpperCase() + newString[word].substring(1);
+        finalString.push(newString[word]);
+      }
+    }
+  finalString[0] = finalString[0][0].toUpperCase() + finalString[0].substring(1);
+  return finalString.join(' ')
+  }
 }
